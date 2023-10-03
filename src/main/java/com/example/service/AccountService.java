8d64a27c8d3dd2e275aa7384000285f04c1fec28
Jason Williams <hfjaybear@gmail.com>
2023-10-03 15:56:22 +0000

    Services Done

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,51 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account persistAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account getAccountById(long id) {
+        Optional<Account> optionalAccount = accountRepository.findById(id);
+
+        if (optionalAccount.isPresent()) {
+            return optionalAccount.get();
+        } else
+            return null;
+    }
+
+    public void deleteAccount(long id){
+        accountRepository.deleteById(id);
+    }
+
+    public void updateAccount(long id, Account replacement){
+        Optional<Account> optionalAccount = accountRepository.findById(id);
+
+        if(optionalAccount.isPresent()){
+            Account account = optionalAccount.get();
+            account.setUsername(replacement.getUsername());
+            account.setPassword(replacement.getPassword());
+        }
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,53 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Message persistMessage(Message message){
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageById(long id){
+        Optional<Message> optionalMessage = messageRepository.findById(id);
+
+        if(optionalMessage.isPresent()){
+            return optionalMessage.get();
+        } else  
+            return null;
+    }
+
+    public void deleteStore(long id){
+        messageRepository.deleteById(id);
+    }
+
+    public Message updateMessage(long id, Message replacement){
+        Optional<Message> optionalMessage = messageRepository.findById(id);
+        Message message = optionalMessage.get();
+
+        if(optionalMessage.isPresent()){
+            message.setMessage_text(replacement.getMessage_text());
+            message.setPosted_by(replacement.getPosted_by());
+            message.setTime_posted_epoch(replacement.getTime_posted_epoch());
+        }
+        return message;
+    }
 }
